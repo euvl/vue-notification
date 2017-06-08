16e86819773925f5b79794812b33399594d3247a
euvl <yev.vlasenko@gmail.com>
2017-06-08 21:58:26 +0100

    Updated demo

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,13 +1,21 @@
 <template>
   <div id="app">
-    <notifications group="foo" position="top left" />
+    <notifications group="foo" position="bottom left" :speed="500" />
     <notifications group="bar"
                    position="bottom right"
                    classes="custom" />
+    <notifications group="baz"
+                   :animation="animation"
+                   position="top left" />
     <div class="content">
-      <button @click="show('foo')">Show top left (basic)</button>
+      <button @click="show('foo')">Show bottom left GENERAL</button>
+      <button @click="show('foo', 'warn')">Show bottom left WARNING</button>
+      <button @click="show('foo', 'error')">Show bottom left ERROR</button>
       <br>
-      <button @click="show('bar')">Show top right (styled, animated)</button>
+      <br>
+      <button @click="show('bar')">Show bottom right</button>
+      <br>
+      <button @click="show('baz')">Show top left</button>
     </div>
   </div>
 </template>
@@ -18,19 +26,26 @@ export default {
   name: 'app',
   data () {
     return {
-      id: 0
+      id: 0,
+      animation: {
+        enter: {
+          opacity: [1, 0],
+          translateX: [0, -300],
+          scale: [1, 0.2]
+        },
+        leave: {
+          opacity: 0
+        }
+      }
     }
   },
   methods: {
-    show (group) {
-      let title = `Test notification #${this.id++}`
-      let text = `
-        Random number: ${Math.random()}
-        <br>
-        Date: ${new Date()}
-      `
+    show (group, type = '') {
+      let title = `Test ${type} notification #${this.id++}`
+      let now = new Date()
+      let text = `This is notification text!<br>Date: ${now}`
 
-      this.$notify({ group, title, text })
+      this.$notify({ group, title, text, type })
     }
   }
 }
@@ -53,7 +68,7 @@ body {
   .content {
     margin: 0 auto;
     width: 300px;
-    
+
     button {
       width: 100%;
     }
@@ -64,15 +79,15 @@ body {
   margin: 5px;
   margin-bottom: 5px;
 
+  border-radius: 5px;
+
   font-size: 13px;
   padding: 10px;
 
-  background: #FCFDFF;
-  border: 1px solid #eaeefb;
-  box-shadow: 0 0 4px 0 rgba(232, 237, 250, 0.6),
-    0 2px 8px 0 rgba(232, 237, 250, 0.5);
-
-  color: #8898A5;
+  color: #FCFDFF;
+  //border: 1px solid #eaeefb;
+  box-shadow: 0 2px 3px 0 rgba(#000000, 0.2);
+  background: #FF8D68;
   // font-weight: 600;
 
   & .notification-title {
