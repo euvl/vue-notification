831739890cce3f28078a2cd8c4dee09e9bec1358
euvl <yev.vlasenko@gmail.com>
2017-06-08 17:28:11 +0100

    New incarnation of the library using Velocity :innocent:

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
-import Component from './Component.vue'
+import Notifications from './Notifications.vue'
+import { events }    from './events'
 
-const plugin = {
+var Notify = {
   install(Vue) {
-
-    const $notify = (params) => {
-      if (typeof params !== 'undefined') {
-        if (typeof params !== 'object') {
-          params = {title: '', text: params}
-        }
-
-        this.events.$emit('add', params)
-      }
+    if (this.installed) {
+      return;
     }
 
-    this.events = new Vue()
-    Object.defineProperty(Vue.prototype, '$notify', {
-      get: () => $notify
-    })
+    this.installed = true;
 
-    Vue.component('notifications', Component)
+    Vue.component('notifications', Notifications)
+    Vue.prototype.$notify = (params) => {
+      if (typeof params === 'string') {
+        params = { title: '', text: params }
+      }
+
+      if (typeof params === 'object') {
+        events.$emit('add', params)
+      }
+    }
   }
 }
 
-export default plugin
+export default Notify;
---
+++ src/util.js
@@ -0,0 +1,2 @@
+export const Id = (i => () => i++)(0)
+export const split = (value) => value.split(/\s+/gi).filter(v => v)
